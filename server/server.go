54dc5b5ad0be95284c78f92f9e29aea28d11b5b1
Brad Reed 😎 <brad@bradreed.co.uk>
2020-10-04 11:38:03 +0100

    Simplify redis dial

--- server/server.go
+++ server/server.go
@@ -30,11 +30,7 @@ func ConnectToRedis(options *options.Options) *redis.Pool {
 		MaxIdle:     3,
 		IdleTimeout: 240 * time.Second,
 		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", options.Redis)
-			if err != nil {
-				return nil, err
-			}
-			return c, err
+			return redis.Dial("tcp", options.Redis)
 		},
 		TestOnBorrow: func(c redis.Conn, t time.Time) error {
 			_, err := c.Do("PING")
